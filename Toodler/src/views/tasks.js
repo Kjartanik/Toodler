c29ed23ca65140f9b517e391c51f064f4779aa3e
KIK <112620098+Kjartanik@users.noreply.github.com>
2024-11-27 11:48:56 +0000

    Update tasks.js

--- Toodler/src/views/tasks.js
+++ Toodler/src/views/tasks.js
@@ -1,1 +1,10 @@
-s
+import React, { useState } from 'react';
+import { View, FlatList, TouchableOpacity, StyleSheet, Text } from 'react-native';
+import BoardCard from '../components/boardCard';
+import { addBoard, deleteBoard, updateBoard } from '../services/dataService';
+import globalStyles from '../styles/globalStyles';
+import data from '../resources/data.json';
+
+const Tasks = ({ navigation }) => {
+    
+}
