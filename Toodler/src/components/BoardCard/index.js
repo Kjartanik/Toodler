70e9a4e782b282356c0c226abbff952932447dbe
KIK <112620098+Kjartanik@users.noreply.github.com>
2024-11-28 10:42:45 +0000

    Refactored Board card to folder with styles and index

--- Toodler/src/components/BoardCard/index.js
+++ Toodler/src/components/BoardCard/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { View, Text, Image, TouchableOpacity, StyleSheet } from 'react-native';
+import { View, Text, Image, TouchableOpacity } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons'; // Import the icon
+import styles from './styles'
 
 const BoardCard = ({ board, onDelete, onModify, onPress, hideActions = false, showTrashIcon = true }) => {
     return (
@@ -38,56 +39,5 @@ const BoardCard = ({ board, onDelete, onModify, onPress, hideActions = false, sh
         </TouchableOpacity>
     );
 };
-
-const styles = StyleSheet.create({
-    card: {
-        marginVertical: 10,
-        padding: 10,
-        backgroundColor: '#f9f9f9',
-        borderRadius: 10,
-    },
-    cardHeader: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-    },
-    image: {
-        width: 100,
-        height: 100,
-        borderRadius: 10,
-    },
-    trashIcon: {
-        padding: 5,
-    },
-    title: {
-        marginTop: 5,
-        fontSize: 18,
-        fontWeight: 'bold',
-    },
-    description: {
-        marginTop: 5,
-        fontSize: 10,
-    },
-    button: {
-        marginTop: 5,
-        padding: 10,
-        backgroundColor: 'pink',
-        borderRadius: 5,
-        alignItems: 'center',
-    },
-    buttonText: {
-        fontSize: 16,
-        fontWeight: 'bold',
-    },
-    iconContainer: {
-        position: 'absolute', // Position icons in the top-right corner
-        top: 10,
-        right: 10,
-        flexDirection: 'row',
-    },
-    icon: {
-        marginLeft: 10, // Spacing between icons
-    },
-});
 
 export default BoardCard;
---
+++ Toodler/src/components/BoardCard/styles.js
@@ -0,0 +1,54 @@
+import { StyleSheet } from 'react-native'
+
+const styles = StyleSheet.create({
+    card: {
+        marginVertical: 10,
+        padding: 10,
+        backgroundColor: '#f9f9f9',
+        borderRadius: 10,
+    },
+    cardHeader: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    },
+    image: {
+        width: 100,
+        height: 100,
+        borderRadius: 10,
+    },
+    trashIcon: {
+        padding: 5,
+    },
+    title: {
+        marginTop: 5,
+        fontSize: 18,
+        fontWeight: 'bold',
+    },
+    description: {
+        marginTop: 5,
+        fontSize: 10,
+    },
+    button: {
+        marginTop: 5,
+        padding: 10,
+        backgroundColor: 'pink',
+        borderRadius: 5,
+        alignItems: 'center',
+    },
+    buttonText: {
+        fontSize: 16,
+        fontWeight: 'bold',
+    },
+    iconContainer: {
+        position: 'absolute', // Position icons in the top-right corner
+        top: 10,
+        right: 10,
+        flexDirection: 'row',
+    },
+    icon: {
+        marginLeft: 10, // Spacing between icons
+    },
+});
+
+export default styles;
